8f805901ca7ceca93a5c78366476baed31924217
Denis <swainet@126.com>
2013-02-04 17:17:24 +0800

    add payfail.html & optimize

--- src/js/widgets/checkbox.js
+++ src/js/widgets/checkbox.js
@@ -2,7 +2,7 @@ define(function (require, exports, module) {
   var $ = require('$');
   $(document).on({
     'tap': function () {
-      if ($(this).hasClass('ui-disabled')) return;
+      if ($(this).hasClass('ui-checkbox-disabled')) return;
       if ($(this).hasClass('ui-checkbox-checked')) {
         $(this).removeClass('ui-checkbox-checked')
         $('input', this).prop('checked', false);
