7de2143724e9f7720809d841a446311bcf84f38e
Denis <swainet@126.com>
2013-02-04 12:04:33 +0800

    update global.js

--- src/js/global.js
+++ src/js/global.js
@@ -1,11 +1,3 @@
 define(function (require, exports, module) {
-  var $ = require('$');
-  $(document).on({
-    'touchstart': function () {
-      $(this).addClass('touch');
-    },
-    'touchend': function () {
-      $(this).removeClass('touch');
-    }
-  }, '.ui-touch');
+
 });
