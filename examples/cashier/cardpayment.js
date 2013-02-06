b16885a7bd498fafbd16565542bf533e590875ef
Denis <swainet@126.com>
2013-02-06 15:25:25 +0800

    delete unneccesary files & add cardpayment-next.html

--- examples/cashier/cardpayment.js
+++ examples/cashier/cardpayment.js
@@ -1,3 +1,4 @@
 define(function (require, exports, module) {
   require('../../src/js/global');
+  require('../../src/js/collapsible');
 });
--- src/js/widgets/select.js
+++ src/js/widgets/select.js
@@ -3,7 +3,7 @@ define(function (require, exports, module) {
   $(document).on({
     'change': function () {
       var container = $(this).closest('.ui-select');
-      $('label', container).text($($('option', this)[this.selectedIndex]).text());
+      $('.ui-select-text', container).text($($('option', this)[this.selectedIndex]).text());
     }
   }, '.ui-select select');
 });
