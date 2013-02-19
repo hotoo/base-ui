1fb25190249c66109ed44a0b2f93dd2583ffb3c4
Denis <swainet@126.com>
2013-02-19 17:56:52 +0800

    add shortcut

--- examples/cashier/cashier.js
+++ examples/cashier/cashier.js
@@ -3,4 +3,5 @@ define(function(require, exports, module) {
   require('../../src/js/widgets/collapsible');
   require('../../src/js/widgets/checkbox');
   require('../../src/js/widgets/select');
+  require('../../src/themes/default/js/widgets/shortcut');
 });
---
+++ src/themes/default/js/widgets/shortcut.js
@@ -0,0 +1,40 @@
+define(function (require, exports, module) {
+  var $ = require('$');
+  var shortcut = $('#shortcut');
+  var shortcutPanel = $('#shortcut-panel');
+  shortcut.on('tap', function (e) {
+    e.stopPropagation();
+    if ($(this).hasClass('active')) {
+      deactiveHandler();
+    } else {
+      activeHandler();
+    }
+  });
+
+  shortcutPanel.on('tap', function (e) {
+    e.stopPropagation();
+  });
+  function deactiveHandler() {
+    if (!shortcutPanel.hasClass('active')) return;
+    shortcut.removeClass('active');
+    shortcutPanel.animate({opacity: 0, scale: 0}, {
+      duration: 200,
+      complete: function () {
+        $(this).hide().removeClass('active');
+      }
+    });
+    $(document).off('tap', deactiveHandler);
+  }
+
+  function activeHandler() {
+    if (shortcutPanel.hasClass('active')) return;
+    shortcut.addClass('active');
+    shortcutPanel.show().animate({opacity: .8, scale: 1}, {
+      duration: 200,
+      complete: function () {
+        $(this).addClass('active');
+      }
+    });
+    $(document).on('tap', deactiveHandler);
+  }
+});
