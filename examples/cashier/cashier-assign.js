5e3c34cc097fea43df090634656cb3031403315f
Denis <swainet@126.com>
2013-02-27 10:31:39 +0800

    支付宝账户标签统一为h3

---
+++ examples/cashier/cashier-assign.js
@@ -0,0 +1,6 @@
+define(function (require, exports, module) {
+  require('../../src/js/global');
+  require('../../src/js/widgets/collapsible');
+  require('../../src/js/widgets/checkbox');
+  require('../../src/js/widgets/select');
+});
