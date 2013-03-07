81a739c817f378460e02db693e94eaaffa110889
Denis <swainet@126.com>
2013-03-07 17:18:07 +0800

    remove windowsphone detect

--- src/js/widgets/detect.js
+++ src/js/widgets/detect.js
@@ -4,14 +4,14 @@ define(function (require, exports, module) {
     iphone = ua.match(/iPhone\sOS\s([\d_]+)/),
     ipad = ua.match(/(iPad).*OS\s([\d_]+)/),
     android = ua.match(/Android\s+([\d.]+)/),
-    windowsphone = ua.match(/Windows\sPhone(?:\sOS)?\s([\d.]+)/),
+  //windowsphone = ua.match(/Windows\sPhone(?:\sOS)?\s([\d.]+)/),
     ie = ua.match(/IEMobile\/([\d.]+)/),
     touch = "ontouchend" in document;
 
   iphone && (os.ios = true, os.version = iphone[1].replace(/_/g, '.'));
   ipad && (os.ios = true, os.version = ipad[1].replace(/_/g, '.'));
   android && (os.android = true, os.version = android[1]);
-  windowsphone && (os.windowsphone = true, os.version = windowsphone[1]);
+  //windowsphone && (os.windowsphone = true, os.version = windowsphone[1]);
   //browser
   ie && (browser.ie = true, browser.version = ie[1]);
   //support
